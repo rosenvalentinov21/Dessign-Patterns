813956a1ab68ddf49c5a6e3fc603161bfcc9c10f
rosen.ivanov <rosen.ivanov721@gmail.com>
2022-10-15 15:55:47 +0300

    Add lifter some functionality

--- src/java/model/Lifter.java
+++ src/java/model/Lifter.java
@@ -5,12 +5,14 @@ public class Lifter extends Person{
     int squatPR;
     int benchPR;
     int deadliftPR;
+    int weight;
 
-    public Lifter(final String firstName,final String lastName,final int age,final int squatPR,final int benchPR,final int deadliftPR) {
+    public Lifter(final String firstName,final String lastName,final int age,final int squatPR,final int benchPR,final int deadliftPR, final int weight) {
         super(firstName, lastName, age);
         this.squatPR = squatPR;
         this.benchPR = benchPR;
         this.deadliftPR = deadliftPR;
+        this.weight = weight;
     }
 
     public int getSquatPR() {
@@ -35,5 +37,17 @@ public class Lifter extends Person{
 
     public void setDeadliftPR(final int deadliftPR) {
         this.deadliftPR = deadliftPR;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getTotal(){
+        return squatPR + benchPR + deadliftPR;
     }
 }
