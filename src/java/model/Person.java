075de1920eaf3ccfaee96fdb0965f507468dd957
rosen.ivanov <rosen.ivanov721@gmail.com>
2022-10-15 15:50:30 +0300

    Defined Person class with base Person attributes

---
+++ src/java/model/Person.java
@@ -0,0 +1,27 @@
+package java.model;
+
+public abstract class Person {
+
+    final String firstName;
+    final String lastName;
+
+    final int age;
+
+    protected Person(final String firstName,final String lastName,final int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
